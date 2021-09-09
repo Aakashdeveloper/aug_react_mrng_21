4c539aed9cfa3e7c167623d39ad344846904aa34
aakashdeveloper <ahanda205@gmail.com>
2021-09-09 08:56:07 +0530

    profile

--- loginapp/src/Component/Profile.js
+++ loginapp/src/Component/Profile.js
@@ -1,10 +1,50 @@
 import React, {Component} from 'react';
+const url = "https://developerjwt.herokuapp.com/api/auth/userinfo"
 
 class Profile extends Component {
+    constructor(props){
+        super(props)
+
+        this.state={
+            user:''
+        }
+    }
+    handleLogout = () => {
+        sessionStorage.removeItem('ltk');
+        this.props.history.push('/')
+    }
+
     render(){
         return(
-            <h1>Profile</h1>
+            <div className="panel panel-warning">
+                <div className="panel-heading">
+                    <h3>User Profile</h3>
+                </div>
+                <div className="panel-body">
+                    <h1>Hi {this.state.user.name}</h1>
+                    <h2>Your EmailId is {this.state.user.email}</h2>
+                    <h2>Your phone is {this.state.user.phone}</h2>
+                    <h2>Your Role is {this.state.user.role}</h2>
+                </div>
+                <button className="btn btn-danger" onClick={this.handleLogout}>
+                    Logout
+                </button>
+            </div>
         )
+    }
+
+    // get user info 
+    componentDidMount(){
+        fetch(url,{
+            method: 'GET',
+            headers: {
+                'x-access-token':sessionStorage.getItem('ltk')
+            }
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            this.setState({user:data})
+        })
     }
 }
 
